e363b0ab8b89e1336f5ab961806a81a61a5ab944
himanshi1810 <98298127+himanshi1810@users.noreply.github.com>
2023-04-05 22:03:25 +0530

    initial commit

---
+++ Code/1.cpp
@@ -0,0 +1,9 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main()
+{
+	char s;
+	cin >> s;
+	cout << "Hello " << s;
+	return 0;
+}
---
+++ Code/c++1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	char c = 'a';
+	cin >> a;
+	cout << "Hello World" << endl;
+	cout << "Hello girls" << endl;
+	cout << "My Name is Himanshi" << endl;
+	cout << a % 10 ;
+	cout << a-- << endl;
+	cout << a++ << endl;
+	cout << ++a << endl;
+	cout << (int)c << endl;
+	
+	
+	return 0;
+}
+
